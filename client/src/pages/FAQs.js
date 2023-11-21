c38c34421186296cc83f5b249a2e6fff66bba720
adityaparmar <adityaparmar9815@gmail.com>
2023-11-21 23:46:36 +0530

    integrated FAQs

--- client/src/pages/FAQs.js
+++ client/src/pages/FAQs.js
@@ -1,37 +1,30 @@
 import Head from "next/head";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
-import {
-  Box,
-  Button,
-  Container,
-  Pagination,
-  Stack,
-  SvgIcon,
-  Typography,
-  Unstable_Grid2 as Grid,
-} from "@mui/material";
+import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid } from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import { AllFAQs } from "src/sections/FAQ/all-FAQs";
+import { Fragment } from "react";
 
 const Page = () => (
-  <>
+  <Fragment>
     <Head>
-      <title>Create FAQs | Thapar Hostel Management System</title>
+      <title>FAQs | Thapar Hostel Management System</title>
     </Head>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
         py: 8,
+        position: "relative",
       }}
     >
       <Container maxWidth="xl">
         <Stack spacing={3}>
-          <Typography variant="h4">Create FAQ</Typography>
+          <Typography variant="h4">FAQs</Typography>
+          <AllFAQs />
         </Stack>
       </Container>
     </Box>
-  </>
+  </Fragment>
 );
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
---
+++ client/src/sections/FAQ/all-FAQs.js
@@ -0,0 +1,78 @@
+import { CircularProgress, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Masonry from "react-masonry-css";
+import { FAQCard } from "src/components/FAQ-card";
+import styles from "../../styles/masonry.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { URL } from "config";
+import axios from "axios";
+import { useState } from "react";
+
+const breakpoints = {
+  default: 3,
+  1200: 2,
+  900: 1,
+};
+
+export const AllFAQs = () => {
+  const [loading, setLoading] = useState(false);
+  const { data: FAQs = [] } = useQuery({
+    queryFn: async () => {
+      setLoading(true);
+      try {
+        const jwt = sessionStorage.getItem("jwt");
+        const url = URL + "dashboard/getFAQ/";
+
+        const getFAQsConfig = {
+          maxBodyLength: Infinity,
+          url: url,
+          headers: {
+            Authorization: "Bearer " + jwt,
+          },
+        };
+
+        const getFAQsResponse = await axios.get(url, getFAQsConfig);
+        setLoading(false);
+        return getFAQsResponse?.data;
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        return [];
+      }
+    },
+    queryKey: ["getFAQs"],
+  });
+
+  return (
+    <Box>
+      {loading ? (
+        <Grid container marginTop="1rem" justifyContent="center">
+          <CircularProgress />
+        </Grid>
+      ) : FAQs.length > 0 ? (
+        <Masonry
+          breakpointCols={breakpoints}
+          className={styles.myMasonryGrid}
+          columnClassName={styles.myMasonryGridColumn}
+        >
+          {FAQs.map(({ question, answer, id }) => (
+            <FAQCard key={id} question={question} answer={answer} sx={{ padding: "2rem" }} />
+          ))}
+        </Masonry>
+      ) : (
+        <Box
+          sx={{
+            position: "absolute",
+            top: "47%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Typography textAlign="center" vatiant="body2" color="grey">
+            No FAQs yet!
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
+};
