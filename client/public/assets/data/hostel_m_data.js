3df2d03d2b5b643caf01a39959ff626ce5111485
adityaparmar <adityaparmar9815@gmail.com>
2024-02-19 03:00:34 +0530

    added rooms in room allocation

---
+++ client/public/assets/data/hostel_m_data.js
@@ -0,0 +1,88 @@
+export const hostelRooms = {
+  rooms: [
+    {
+      id: "M101",
+      roomType: "Single",
+      price: "1000",
+      description:
+        "Single room with a single bed, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M102",
+      roomType: "Double",
+      price: "1500",
+      description:
+        "Double room with two single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M103",
+      roomType: "Triple",
+      price: "2000",
+      description:
+        "Triple room with three single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M104",
+      roomType: "Quad",
+      price: "2500",
+      description:
+        "Quad room with four single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M105",
+      roomType: "Single",
+      price: "1000",
+      description:
+        "Single room with a single bed, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M106",
+      roomType: "Double",
+      price: "1500",
+      description:
+        "Double room with two single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M107",
+      roomType: "Triple",
+      price: "2000",
+      description:
+        "Triple room with three single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M108",
+      roomType: "Quad",
+      price: "2500",
+      description:
+        "Quad room with four single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M109",
+      roomType: "Single",
+      price: "1000",
+      description:
+        "Single room with a single bed, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M110",
+      roomType: "Double",
+      price: "1500",
+      description:
+        "Double room with two single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M111",
+      roomType: "Triple",
+      price: "2000",
+      description:
+        "Triple room with three single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+    {
+      id: "M112",
+      roomType: "Quad",
+      price: "2500",
+      description:
+        "Quad room with four single beds, a desk, a chair, a wardrobe, and a private bathroom.",
+    },
+  ],
+};
--- client/src/pages/room-booking.js
+++ client/src/pages/room-booking.js
@@ -1,15 +1,35 @@
 import Head from "next/head";
-import { Box, Container, Unstable_Grid2 as Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Unstable_Grid2 as Grid,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import Hostel_M_Map from "public/assets/hostels/Hostel_M_Map.jpg";
 import Image from "next/image";
+import { hostelRooms } from "public/assets/data/hostel_m_data.js";
 
 const Page = () => {
   const queryClient = useQueryClient();
   const user = queryClient.getQueryData(["getProfile"]);
   console.log(user);
+
+  console.log(hostelRooms);
+  const [floor, setFloor] = useState("ground");
+
+  const handleChange = (event) => {
+    setFloor(event.target.value);
+  };
+
+  console.log(hostelRooms);
 
   return (
     <Fragment>
@@ -24,17 +44,47 @@ const Page = () => {
         }}
       >
         <Container maxWidth="xl">
-          <Typography variant="h3" sx={{ mb: 3 }} paddingLeft={2}>
-            Hostel O
-          </Typography>
+          <Grid container justifyContent={"space-between"}>
+            <Grid item>
+              <Typography variant="h3" sx={{ mb: 3 }} paddingLeft={2}>
+                Hostel M
+              </Typography>
+            </Grid>
+            <Grid item paddingRight={2}>
+              <Select
+                labelId="select-floor-label"
+                id="select-floor"
+                name="floor"
+                label="Choose Floor"
+                value={floor}
+                onChange={handleChange}
+                autoWidth
+                sx={{ mb: 2 }}
+                inputProps={{ IconComponent: () => null }}
+              >
+                <MenuItem value="ground">Ground Floor</MenuItem>
+                <MenuItem value="first">First Floor</MenuItem>
+                <MenuItem value="second">Second Floor</MenuItem>
+              </Select>
+            </Grid>
+          </Grid>
 
           <Grid container spacing={3}>
             <Grid item xs={12} alignSelf={"center"}>
               <Image
                 src={Hostel_M_Map}
                 alt="Alloted Hostel Map"
-                style={{ maxWidth: "100%", height: "auto" }}
+                style={{ maxWidth: "100%", height: "auto", borderRadius: "8px" }}
               />
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container>
+                {hostelRooms.rooms.map((room) => (
+                  <Grid key={room.id} item>
+                    <Button variant="outlined">{room.id}</Button>
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
           </Grid>
         </Container>
