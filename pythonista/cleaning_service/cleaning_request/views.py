b80512d20a636952d910f5b709be0d21a2771281
Chandravo <chandravob2003@gmail.com>
2024-09-01 23:47:16 +0530

    added pagination

--- pythonista/cleaning_service/cleaning_request/views.py
+++ pythonista/cleaning_service/cleaning_request/views.py
@@ -2,25 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+
 from config.services import *
 from .serializers import *
+
+from config.pagination import ResponsePagination
+
 class getCleaningRequests(APIView):
     def get(self, request):
-        requests = all_objects(CleaningRequest.objects)
-        # data = []
-        # for request in requests:
-        #     data.append({
-        #         'id': request.id,
-        #         'student': request.student,
-        #         'worker': request.worker,
-        #         'slot': request.slot,
-        #         'hostel_id': request.hostel_id,
-        #         'block': request.block,
-        #         'room_number': request.room_number,
-        #         'status': request.status
-        #     })
-        print(requests)
-        serializer = CleaningRequestSerializer(requests, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        cleaning_requests = all_objects(CleaningRequest.objects)
+        # print(requests)
+        paginator = ResponsePagination()
+        paginated_queryset = paginator.paginate_queryset(cleaning_requests, request)
+        
+        serializer = CleaningRequestSerializer(paginated_queryset, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
     
