382792c56d639523bdda5492e54b1eb9dfdc886e
Arvinder Singh Kandola <76721224+askandola@users.noreply.github.com>
2024-09-03 01:01:14 +0530

    Modify api response for profile view in central repo service

--- pythonista/central_repository/user/serializers.py
+++ pythonista/central_repository/user/serializers.py
@@ -13,22 +13,30 @@ class StudentProfileSerializer(serializers.ModelSerializer):
     email = serializers.SlugRelatedField(
         slug_field="email", read_only=True, source="user"
     )
-    room_details = serializers.SerializerMethodField()
+    room = serializers.SerializerMethodField()
 
-    def get_room_details(self, obj):
-        return {
-            "room": obj.room.name,
-            "level": obj.room.level.name if obj.room.level is not None else None,
-            "block": obj.room.block.name if obj.room.block is not None else None,
-            "room_type": (
-                obj.room.room_type.name if obj.room.room_type is not None else None
-            ),
-            "hostel": (
-                obj.room.level.hostel.name
-                if obj.room.level is not None and obj.room.level.hostel is not None
-                else None
-            ),
-        }
+    def get_room(self, obj):
+        return (
+            {
+                "id": obj.room.id,
+                "name": obj.room.name,
+                "level": (obj.room.level.name if obj.room.level is not None else None),
+                "block": (obj.room.block.name if obj.room.block is not None else None),
+                "room_type": (
+                    obj.room.room_type.name if obj.room.room_type is not None else None
+                ),
+                "hostel": (
+                    {
+                        "id": obj.room.level.hostel.id,
+                        "name": obj.room.level.hostel.name,
+                    }
+                    if obj.room.level is not None and obj.room.level.hostel is not None
+                    else None
+                ),
+            }
+            if obj.room is not None
+            else None
+        )
 
     class Meta:
         model = Student
@@ -39,7 +47,7 @@ class StudentProfileSerializer(serializers.ModelSerializer):
             "email",
             "phone_number",
             "branch",
-            "room_details",
+            "room",
         )
 
 
@@ -47,7 +55,19 @@ class SupervisorProfileSerializer(serializers.ModelSerializer):
     email = serializers.SlugRelatedField(
         slug_field="email", read_only=True, source="user"
     )
-    hostel = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    hostel = serializers.SerializerMethodField()
+
+    def get_hostel(self, obj):
+        return (
+            (
+                {
+                    "id": obj.hostel.id,
+                    "name": obj.hostel.name,
+                }
+            )
+            if obj.hostel is not None
+            else None
+        )
 
     class Meta:
         model = Supervisor
