0e5ac346897242cc46e6b99c32fcfd1c9d4eec6c
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-09-03 23:59:48 +0530

    made specialbutton

--- subadmin_client/src/components/cleaning/Assignment/Requests.tsx
+++ subadmin_client/src/components/cleaning/Assignment/Requests.tsx
@@ -14,29 +14,29 @@ export function Requests({ selectedCleaner }: RequestsProps): React.JSX.Element 
   const [cleaningRequests, setCleaningRequests] = React.useState<RequestProps[]>(tempRequests);
 
   return (
-    <Paper elevation={10} sx={{ p: 2, width: 0.65 }}>
+    <Paper elevation={10} sx={{ p: 3, width: 0.65 }}>
       <Stack direction="row" justifyContent="space-between">
         <Typography variant="h5">Cleaning Requests</Typography>
         <Box>Filters</Box>
       </Stack>
       <Box mt={3} sx={{ overflowY: 'auto', height: '47vh', width: 1 }}>
-        {cleaningRequests.map((request) => {
+        {cleaningRequests.map((request, index) => {
           return (
             <Box key={request.id}>
-              <Stack direction="row" py={2} justifyContent="space-evenly" alignItems="center">
-                <Typography variant="h5" fontWeight={500}>
+              <Stack direction="row" py={1} justifyContent="flex-start" gap={9} alignItems="center">
+                <Typography variant="h6" fontSize="20px" fontWeight={500}>
                   {request.roomName}
                 </Typography>
 
                 <SpecialButton slots={request.slots} />
               </Stack>
-              <Divider sx={{ my: 0.4 }} />
+              {cleaningRequests.length - 1 !== index && <Divider sx={{ my: 0.4 }} />}
             </Box>
           );
         })}
       </Box>
-      <Divider sx={{ my: 2 }} />
-      <Stack direction="row" justifyContent="space-between" mt={2} width={1}>
+
+      <Stack direction="row" justifyContent="space-between" mt={4} width={1}>
         <Typography variant="body1">Select a worker to assign cleaning</Typography>
         <Button variant="contained" sx={{ px: 6 }} color="primary">
           Save
@@ -46,33 +46,83 @@ export function Requests({ selectedCleaner }: RequestsProps): React.JSX.Element 
   );
 }
 
-function SpecialButton({ Slots }: { slots: { from: string; to: string }[] }): React.JSX.Element {
-  const timings = [10, 11, 12, 1, 2, 3, 4, 5];
+
+
+
+function SpecialButton({ slots }: { slots: { from: string; to: string }[] }): React.JSX.Element {
+  const timings = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
+  const activeSlots = slots.map((slot) => {
+    const from = new Date(slot.from).getHours();
+    const to = new Date(slot.to).getHours();
+    return timings.slice(from - 9, to - 9);
+  });
 
   return (
     <Box>
-      <ButtonGroup sx={{ borderRadius: 0 }}>
-        {timings.map((time) => {
+      <ButtonGroup
+        sx={{
+          borderRadius: 0,
+          '& .MuiButtonGroup-grouped': {
+            borderColor: 'transparent',
+            borderRight: 'none',
+          },
+          width: 1,
+        }}
+        variant="text"
+      >
+        {timings.map((time, index) => {
           const [selectSlot, setSelectSlot] = React.useState(false);
+          const dividerColorCondition=activeSlots.flat().includes(time)||(
+            activeSlots.flat().includes(time-1)&& time!==5) || ( time===1&& activeSlots.flat().includes(12));
+
           return (
-            <Button
-              key={time}
-              sx={{
-                m:0,
-                px: selectSlot ? 3.34 : 4,
-                py: selectSlot ? 0.5 : 2,
-                height: 1,
-                borderRadius: 0,
-                borderBlock: 'transparent',
-                fontWeight: 800,
-              }}
-              onClick={() => {
-                setSelectSlot(!selectSlot);
-              }}
-              color="primary"
-            >
-              {selectSlot ? '✓' : ''}
-            </Button>
+            <Stack key={time} direction="row" alignItems="end" justifyContent="center">
+
+              <Stack direction="column" alignItems="center" width="14px">
+                <Typography variant="caption" sx={{ p: 0, color: 'var(--TextMain-Color)' }}>
+                  {time}
+                </Typography>
+
+                <Divider
+                  sx={{
+                    backgroundColor: index === 0 ? 'transparent' : 'var(--mui-palette-secondary-main)',
+                    borderWidth: index === 0 ? 0 : '1px',
+                    borderStyle: 'solid',
+                    borderColor: dividerColorCondition?'var(--mui-palette-primary-main)':'var(--mui-palette-secondary-main)',
+                    m: 0,
+                    mt: 1,
+                    py: 2,
+                  }}
+                  variant="middle"
+                  orientation="vertical"
+                />
+              </Stack>
+
+              <Button
+                key={time}
+                sx={{
+                  m: 0,
+                  px:selectSlot?{xl:3.34,lg:1}:{xl:4,lg:1},
+                  py: 2,
+                  height: 0.3,
+                  borderRadius: 0,
+                  fontWeight: 800,
+                  backgroundColor: activeSlots.flat().includes(time) ?'#F4C9C9':'transparent',
+                  pointerEvents: activeSlots.flat().includes(time) ? 'auto' : 'none',
+                  '&:hover': {
+                    backgroundColor: activeSlots.flat().includes(time) ?'#F4C9C9':'transparent',
+                  }
+                }}
+                onClick={() => {
+                  setSelectSlot(!selectSlot);
+                }}
+                color="primary"
+              >
+                {selectSlot ? '✓' : ''}
+              </Button>
+
+            </Stack>
           );
         })}
       </ButtonGroup>
