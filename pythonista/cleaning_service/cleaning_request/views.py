3163ed79e717772f067cf204f9c68b2d43bc8d94
Chandravo <chandravob2003@gmail.com>
2024-09-03 15:09:32 +0530

    crud update

--- pythonista/cleaning_service/cleaning_request/views.py
+++ pythonista/cleaning_service/cleaning_request/views.py
@@ -3,15 +3,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from .serializers import *
 
 from config.services import *
-from .serializers import *
 
 from config.pagination import ResponsePagination
 
 class getCleaningRequests(APIView):
     def get(self, request):
-        cleaning_requests = all_objects(CleaningRequest.objects)
+        filters = request.query_params.dict()
+        cleaning_requests = filter_objects(CleaningRequest.objects, **filters)
         # print(requests)
         paginator = ResponsePagination()
         paginated_queryset = paginator.paginate_queryset(cleaning_requests, request)
@@ -20,3 +21,19 @@ class getCleaningRequests(APIView):
         return paginator.get_paginated_response(serializer.data)
     
     
+class getSingleCleaningRequest(APIView):
+    def get(self, request, slug):
+        #todo: get the student details and return it
+        cleaning_request = get_object(CleaningRequest.objects, id=slug)
+        serializer = CleaningRequestSerializer(cleaning_request)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class createCleaningRequests(APIView):
+    def post(self, request):
+        #student id
+        data = request.data
+        serializer = CleaningRequestSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- pythonista/cleaning_service/slots/views.py
+++ pythonista/cleaning_service/slots/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from config.services import *
+from serializers import *
 
-# Create your views here.
+class getSlots(APIView):
+    def get(self, request):
+        #user = request.user
+        #slots = all_objects(Slot.objects, hostel_id=user.student.hostel.id)
+        slots = filter_objects(Slot.objects, hostel_id = request.data.get('hostel_id')) # to be replaced later after integration
+        serializer = SlotSerializer(slots, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+class createSlot(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = SlotSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
