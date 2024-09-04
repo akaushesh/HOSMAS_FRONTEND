a44ac53992d6e90f9bd4659c606564a10f2c65bd
Chandravo <chandravob2003@gmail.com>
2024-09-05 00:57:55 +0530

    integrated authentication in views

--- pythonista/cleaning_service/cleaning_request/urls.py
+++ pythonista/cleaning_service/cleaning_request/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('getCleaningRequests/', getCleaningRequests.as_view()),
     path('getCleaningRequests/<int:slug>/', getSingleCleaningRequest.as_view()),
+    path('createCleaningRequests/', createCleaningRequests.as_view()),
 ]
