d8b41460dddc58566d681a028b0249ef0905e96d
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-09-19 02:54:45 +0530

    Cleaner client updated

--- cleaner_client/src/app/layout.tsx
+++ cleaner_client/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +29,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Box
+          sx={{
+            bgcolor: "var(--mui-palette-background-default)",
+            display: "flex",
+            flexDirection: "column",
+            position: "relative",
+            minHeight: "100vh",
+          }}
+        >
+          {/* <Box sx={{ position: 'absolute', top: 0, left: '35%', zIndex: 0 }}>
+          <Ellipse2 />
+        </Box> */}
+
+          {/* <Box  sx={{position:"absolute",bottom:0,left:"60%",zIndex:0}}>
+          <Ellipse1/>
+        </Box> */}
+
+          <Box
+            sx={{
+              display: "flex",
+              flex: "1 1 auto",
+              flexDirection: "column",
+            }}
+          >
+            <main>
+              <Container maxWidth="xl" sx={{ py: "64px" }}>
+                {children}
+              </Container>
+            </main>
+          </Box>
+        </Box>
       </body>
     </html>
   );
---
+++ cleaner_client/src/components/cleaners/CleanerHostels.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import * as React from "react";
+import { Stack } from "@mui/system";
+
+import hostels from "./assets/HostelData";
+import CleanerCards from "./cleaner-cards";
+import { tempCleaners } from "./assets/CleanersData";
+import { Box, Typography } from "@mui/material";
+import {
+  getCleanersFromHostel,
+  type CleanersResponse,
+} from "@/services/cleaning";
+import { getAllHostels, Root } from "@/services/hostel";
+
+export default function CleanerHostels(): React.JSX.Element {
+  const [pageState, setPageState] = React.useState(0);
+  const [hostel, setHostel] = React.useState("");
+
+  // const [hostels, setHostels] = React.useState<Root>([]);
+
+  // React.useEffect(() => {
+  //   const fetchHostels = async () => {
+  //     const hostels = await getAllHostels();
+  //     console.log(hostels.data);
+  //     setHostels(hostels.data);
+  //   };
+  //   fetchHostels();
+  // }, []);
+
+  // const [cleaners, setCleaners] = React.useState<CleanersResponse>([]);
+
+  // React.useEffect(() => {
+  //   const fetchCleaners = async () => {
+  //     const cleaners = await getCleanersFromHostel(hostel);
+  //     console.log(cleaners.data);
+  //     setCleaners(cleaners.data);
+  //   };
+  //   if (pageState === 1) {
+  //     fetchCleaners();
+  //   }
+  // }, [pageState]);
+
+  const cleaners = tempCleaners;
+  const hostelsData = hostels;
+
+  return (
+    <Stack
+      sx={{
+        "--Page-HeadColor": "var(--mui-palette-text-secondaryChannel)",
+        "--TextMain-Color": "var(--mui-palette-text-primary)",
+        "--PButton-Color": "var(--mui-palette-primary-main)",
+        "--PButton-HoverColor": "var(--mui-palette-primary-dark)",
+        "--SButton-Color": "var(--mui-palette-secondary-dark)",
+        "--SButton-HoverColor": "var(--mui-palette-secondary-main)",
+        "--Button-FontColor": "var(--mui-palette-common-white)",
+        "--Room-Available": "transparent",
+        "--Room-Allotted": "#32a83c",
+        "--Room-Color": "var(--mui-palette-secondary-main)",
+      }}
+    >
+      {pageState === 0 && (
+        <Box>
+          <Typography
+            variant="body1"
+            sx={{
+              fontSize: { xs: "15px", md: "22px" },
+              mt: 0.1,
+              color: "var(--TextMain-Color)",
+            }}
+          >
+            Choose Hostel
+          </Typography>
+
+          <CleanerCards
+            data={hostelsData}
+            setHostel={setHostel}
+            pageState={pageState}
+            setPageState={setPageState}
+          />
+        </Box>
+      )}
+
+      {pageState === 1 && (
+        <Box>
+          <Typography
+            variant="body1"
+            sx={{
+              fontSize: { xs: "15px", md: "22px" },
+              mt: 0.1,
+              color: "var(--TextMain-Color)",
+            }}
+          >
+            Choose Cleaner
+          </Typography>
+
+          <CleanerCards
+            data={cleaners}
+            setHostel={setHostel}
+            pageState={pageState}
+            setPageState={setPageState}
+          />
+        </Box>
+      )}
+    </Stack>
+  );
+}
