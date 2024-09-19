f59f01665f5f56ea2c4aaf9da2519fa9af2fe2dc
Arvinder Singh Kandola <76721224+askandola@users.noreply.github.com>
2024-09-19 10:00:25 +0530

    Modify get all workers and mark cleaning request done api endpoint

--- pythonista/cleaning_service/worker/urls.py
+++ pythonista/cleaning_service/worker/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('workers/<int:worker_id>/mark-attendance/', markWorkerAttendance.as_view()),
-    path('getWorkers/', getMultipleWorkers.as_view()),
-    path('getWorkers/<int:slug>/', getSingleWorker.as_view()),
     path('createWorker/', createWorker.as_view()),
 
     path('hostels/<int:hostel_id>/workers/', HostelWorkersPublicView.as_view()),
+    path('workers/<int:worker_id>/', getSingleWorker.as_view()),
+
+    path('workers/<int:worker_id>/mark-attendance/', markWorkerAttendance.as_view()),
 ]
