e4c381f294bd42138e52fa506752f031e0d0bbcc
akaushesh <anik.kaushesh@gmail.com>
2026-07-16 03:21:58 +0530

    feat(api): standardize API URLs across client services

--- subadmin_client/src/services/api.ts
+++ subadmin_client/src/services/api.ts
@@ -17,13 +17,6 @@ export const messUrl = 'https://mess.hosmas.ccstiet.com/';
 // export const messUrl = 'http://localhost:6555/';
 // export const leaveUrl = 'http://localhost:6699/';
 
-// export const url = 'http://13.50.249.120:3376/';
-// export const centralUrl = 'http://13.50.249.120:3376/';
-// export const cleaningUrl = 'http://13.50.249.120:3378/';
-// export const laundryUrl = 'http://13.50.249.120:3388/';
-// export const allocationUrl = 'http://13.50.249.120:6543/';
-// export const messUrl = 'http://13.50.249.120:6555/';
-// export const leaveUrl = 'http://13.50.249.120:6699/';
 
 export const authApi = axios.create({ baseURL: url });
 export const allocationApi = axios.create({ baseURL: allocationUrl });
