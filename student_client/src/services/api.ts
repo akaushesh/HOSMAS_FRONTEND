1265be118b6fcb3ee30c738df3a016a8299f0d26
akaushesh <anik.kaushesh@gmail.com>
2026-07-16 03:21:06 +0530

    feat(api): standardize API URLs across client services

--- student_client/src/services/api.ts
+++ student_client/src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// export const url = 'https://central.hosmas.ccstiet.com/';
-// export const allocationUrl = 'https://api.hosmas.ccstiet.com/';
-// export const centralUrl = 'https://central.hosmas.ccstiet.com/';
-// export const cleaningUrl = 'https://cleaning.hosmas.ccstiet.com/';
-// export const laundryUrl = 'https://hosmas-laundry.ccstiet.com/';
-// export const leaveUrl = 'https://hosmas-mess-leave.ccstiet.com/';
-// export const messUrl = 'https://hosmas-mess-leave.ccstiet.com/';
+export const url = 'https://central.hosmas.ccstiet.com/';
+export const centralUrl = 'https://central.hosmas.ccstiet.com/';
+export const allocationUrl = 'https://allocation.hosmas.ccstiet.com';
+export const cleaningUrl = 'https://cleaning.hosmas.ccstiet.com/';
+export const laundryUrl = 'https://laundry.hosmas.ccstiet.com/';
+export const leaveUrl = 'https://leave.hosmas.ccstiet.com/';
+export const messUrl = 'https://mess.hosmas.ccstiet.com/';
 
 
 // export const url = 'http://localhost:3376/';
@@ -17,13 +17,6 @@ import axios from 'axios';
 // export const messUrl = 'http://localhost:6555/';
 // export const leaveUrl = 'http://localhost:6699/';
 
-export const url = 'http://13.50.249.120:3376/';
-export const centralUrl = 'http://13.50.249.120:3376/';
-export const cleaningUrl = 'http://13.50.249.120:3378/';
-export const laundryUrl = 'http://13.50.249.120:3388/';
-export const allocationUrl = 'http://13.50.249.120:6543/';
-export const messUrl = 'http://13.50.249.120:6555/';
-export const leaveUrl = 'http://13.50.249.120:6699/';
 
 export const tempApi = axios.create({ baseURL: allocationUrl });
 export const authApi = axios.create({ baseURL: centralUrl });
